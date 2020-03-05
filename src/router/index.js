9d204975dda7acbc5e16fdb44b76fad2c5bd3be8
lsc16 <lsc980106>
2020-03-05 17:25:08 +0800

    测试数据

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "首页",
+    component: () => import("../views/Home.vue"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "登录",
+    component: () => import("../views/About.vue")
   }
 ];
 
